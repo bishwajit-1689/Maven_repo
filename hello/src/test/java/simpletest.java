2d3c4178b393aca8e3de5490e34079efb1ffc62c
root <root@ci_cd-devops.testlab.com>
2017-11-25 13:24:45 +0530

    First Maven project

---
+++ hello/src/test/java/simpletest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class simpletest {
+
+    @Test
+    public void simpletestpass() {
+
+        int a= 2;
+        int b = 2;
+        assertTrue(a + b ==4);
+
+
+        }
+    }
+
